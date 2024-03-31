1f6645acb27d371d5eb5948b193859f160f175e2
Nikita Konstantinov <110055310+JohnyRico2022@users.noreply.github.com>
2024-03-31 12:35:46 +0300

    lesson9. changed about

--- app/src/main/java/ru/nikita/cleanarchitecturebasic/About.kt
+++ app/src/main/java/ru/nikita/cleanarchitecturebasic/About.kt
@@ -3,8 +3,11 @@ package ru.nikita.cleanarchitecturebasic
 /*
   Лекции часть 9:  MVI
 
+  MVI (Model-View-Intent) — это архитектурный паттерн,
+  который входит в семейство паттернов Unidirectional Data Flow — подход к проектированию системы,
+  в котором всё представляется в виде однонаправленного потока действий и управления состоянием.
 
-  Между Activity  и ViewModel один вход и один выход
+
 
    Диаграмма MVI: app/src/main/res/drawable
 
